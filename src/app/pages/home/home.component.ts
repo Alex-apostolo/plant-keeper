077f4b2d5b33417db3e30796e92841c7cde21cfd
Andreas Lemesios <alemesios@gmail.com>
2021-04-19 21:22:41 +0300

    home page profiles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,87 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  keywordExists: boolean = true;
+  keepers = [{ name: '', location: '', rating: 0, url: '' }];
+  tempkeepers = [{ name: '', location: '', rating: 0, url: '' }];
 
   ngOnInit(): void {
+    this.keepers = [
+      {
+        name: 'Andreas Lemesios',
+        location: 'Larnaca',
+        rating: 5,
+        url: 'assets/andreas.jpg',
+      },
+      {
+        name: 'Lea Baviere',
+        location: 'Bath',
+        rating: 5,
+        url: 'assets/lea.jpg',
+      },
+      {
+        name: 'Kiara Polychroniadi',
+        location: 'Bath',
+        rating: 5,
+        url: 'assets/kiara.jpg',
+      },
+      {
+        name: 'Alex Apostolopoulos',
+        location: 'Athens',
+        rating: 1,
+        url: 'assets/alex.jpg',
+      },
+      {
+        name: 'Alex Apostolopoulos',
+        location: 'Athens',
+        rating: 1,
+        url: 'assets/alex.jpg',
+      },
+      {
+        name: 'Alex Apostolopoulos',
+        location: 'Athens',
+        rating: 1,
+        url: 'assets/alex.jpg',
+      },
+      {
+        name: 'Alex Apostolopoulos',
+        location: 'Athens',
+        rating: 1,
+        url: 'assets/alex.jpg',
+      },
+      {
+        name: 'Alex Apostolopoulos',
+        location: 'Athens',
+        rating: 1,
+        url: 'assets/alex.jpg',
+      },
+      {
+        name: 'Alex Apostolopoulos',
+        location: 'Athens',
+        rating: 1,
+        url: 'assets/alex.jpg',
+      },
+    ];
+    this.tempkeepers = this.keepers;
   }
 
+  onSearch(keyword: string) {
+    if (keyword == '') {
+      this.tempkeepers = this.keepers;
+      this.keywordExists = true;
+      return;
+    }
+    this.tempkeepers = [];
+    this.keepers.forEach((dict) => {
+      if (dict['location'] == keyword) {
+        this.tempkeepers.push(dict);
+      }
+    });
+    if (this.tempkeepers.length > 0) this.keywordExists = true;
+    else this.keywordExists = false;
+  }
 }
