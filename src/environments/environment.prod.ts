0c6b5df46a17f54206fd52e111ec44be98d064b2
Andreas Lemesios <alemesios@gmail.com>
2021-04-21 23:06:33 +0300

    temp fix for ng build --prod (Attempt #2)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+};
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCbt1M1m2Aa_nbYAJz1oj-l1ZGEK995oxg',
+  authDomain: 'plantkeeper-4cedb.firebaseapp.com',
+  databaseURL: 'https://plantkeeper-4cedb-default-rtdb.firebaseio.com',
+  projectId: 'plantkeeper-4cedb',
+  storageBucket: 'plantkeeper-4cedb.appspot.com',
+  messagingSenderId: '623507312934',
+  appId: '1:623507312934:web:c0e3d0fd10c9c12ca471e7',
 };
